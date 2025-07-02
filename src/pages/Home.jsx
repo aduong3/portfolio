9e15b2c3864a2c2df8662c6b5996696775431322
aduong3 <mythalink@gmail.com>
2025-07-02 15:42:37 -0700

    (add): Added non transparent BG photo for profile picture style in smaller screen sizes.
    Home page is complete, need to adjust Sidebar to become a collapsable menu of some sort in smaller screen sizes.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import photo from "../assets/Andrew_Lake_Tahoe_NOBG.png";
+import photoNoBG from "../assets/Andrew_Lake_Tahoe_NOBG.png";
+import photo from "../assets/Andrew_Lake_Tahoe.png";
 import { IoMdDownload } from "react-icons/io";
 import { MdOutlineEmail } from "react-icons/md";
 import { Link } from "react-router";
@@ -6,17 +7,26 @@ import { Link } from "react-router";
 function Home() {
   return (
     <div className="flex h-full items-center justify-center">
-      <section className="mr-96 flex flex-col items-center justify-center gap-8 text-center">
-        <h1 className="text-7xl">Andrew Duong</h1>
-        <h2 className="text-4xl text-blue-400">Software Engineer</h2>
+      <section className="flex flex-col items-center justify-center gap-4 text-center xl:mr-96 xl:gap-8">
+        <div className="flex h-[15em] w-[15em] items-center justify-center overflow-hidden rounded-full border-4 border-blue-500 xl:hidden">
+          <img
+            src={photo}
+            alt="Photo of Andrew Duong profile size"
+            className="scale-150"
+          />
+        </div>
+        <h1 className="text-5xl lg:text-7xl">Andrew Duong</h1>
+        <h2 className="text-3xl text-blue-400 lg:text-4xl">
+          Software Engineer
+        </h2>
         <div className="w-[42%]">
-          <p className="text-xl">
+          <p className="text-md lg:text-xl">
             I’m a Full Stack Developer passionate about learning new
             technologies and turning them into solutions that make a difference.
           </p>
         </div>
 
-        <div className="flex gap-24 text-xl">
+        <div className="flex flex-col items-center gap-4 text-xl md:flex-row md:gap-24">
           <a
             className="rounded-lg bg-yellow-300 px-3 py-1 transition-all duration-300 hover:-translate-y-1 hover:cursor-pointer hover:shadow-[6px_6px_5px_rgba(0,0,0,.20)]"
             href="./Andrew_Duong_Resume.pdf"
@@ -40,9 +50,9 @@ function Home() {
       </section>
 
       <img
-        src={photo}
+        src={photoNoBG}
         alt="Photo of Andrew Duong"
-        className="pointer-events-none fixed right-[-225px] bottom-0 drop-shadow-[25px_12px_0_rgba(96,165,250)]"
+        className="pointer-events-none fixed right-[-225px] bottom-0 hidden drop-shadow-[25px_12px_0_rgba(96,165,250)] xl:inline"
       />
     </div>
   );
